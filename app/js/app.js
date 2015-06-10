a26c02440e9b53e57101e517af8148b2c112753a
insertvalue <503170163@qq.com>
2015-06-10 10:47:06 +0800

    前端文件

--- app/js/app.js
+++ app/js/app.js
@@ -1,29 +1,41 @@
 'use strict';
 
 /* App Module */
-
+Headroom.options.offset = 200;
+Headroom.options.classes = {
+    initial: "animated",
+    pinned: "fadeInDown",
+    unpinned: "fadeOutUp",
+    top: 'headroom--top',
+    notTop: 'headroom--not-top'
+};
 var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatControllers',
-  'phonecatFilters'
-  ]);
+    'ngRoute',
+    'phonecatControllers',
+    'phonecatFilters',
+    'headroom'
+]);
 
 phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-    when('/phones', {
-      templateUrl: 'partials/phone-list.html',
-      controller: 'PhoneListCtrl'
-    }).
-    when('/phones/:phoneId', {
-      templateUrl: 'partials/phone-detail.html',
-      controller: 'PhoneDetailCtrl'
-    }).
-    when('/highcharts', {
-      templateUrl: 'partials/highcharts-test.html',
-      controller: 'HighchartsCtrl'
-    }).
-    otherwise({
-      redirectTo: '/phones'
-    });
-  }]);
+    function ($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'partials/login.html',
+                controller: 'LoginCtrl'
+            }).
+            when('/phones', {
+                templateUrl: 'partials/phone-list.html',
+                controller: 'PhoneListCtrl'
+            }).
+            when('/phones/:phoneId', {
+                templateUrl: 'partials/phone-detail.html',
+                controller: 'PhoneDetailCtrl'
+            }).
+            when('/highcharts', {
+                templateUrl: 'partials/highcharts-test.html',
+                controller: 'HighchartsCtrl'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    }]);
